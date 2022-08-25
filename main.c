291e391e7e112c3bc45a628675636275fadbb4ac
Charles Eduardo <charleseduardofaria@gmail.com>
2022-08-24 23:34:54 -0300

    Arrumando codigo

--- main.c
+++ main.c
@@ -1,64 +1,61 @@
 #include <stdio.h>
-#include <locale.h>
 
 int main() {
+    int op = menu_principal();
 
-    menu_Principal();
-
-    int opcao = 0;
-    printf("Digite uma op��o:\n");
-    scanf("%i", &opcao);
-
-    if (opcao == 4)
-    {
-        menu_Sobre();
+    if (op == 4) {
+        menu_sobre();
     }
     
-    
     return 0;
 }
 
+int menu_principal(void) {
+    system("cls");
 
-int menu_Principal(){
-    setlocale (LC_ALL, "portuguese");
+    printf("\t\t=====================================\n");
+    printf("\t\t||           SIG-Customer          ||\n");
+    printf("\t\t=====================================\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|       1 - Modulo de Cliente       |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   2 - Modulo em Desenvolvimento   |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   3 - Modulo em Desenvolvimento   |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|        4 - Sobre e Equipe         |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|           0 - Encerrar            |\n");
+    printf("\t\t-------------------------------------\n");
 
-    printf("\n"
-    "\t\t==============================\n"
-    "\t\tPrograma Controle de Financias\n"
-    "\t\t==============================\n"
-    "\t\t1 - M�dulo clientes\n"
-    "\t\t2 - M�dulo em desenvolvimento\n"
-    "\t\t3 - M�dulo em desenvolvimento\n"
-    "\t\t4 - Sobre e equipe\n"
-    "\t\t0 - Encerrar\n"
-    "\n"
-    );
+    int op;
 
-    
+    scanf("%d", &op);
 
-    return 0;
+    return op;
 }
 
-int menu_Sobre(){
-    setlocale (LC_ALL, "portuguese");
+void menu_sobre(void) {
+    system("cls");
+    printf("\n\n");
+    printf("\t\tSobre o projeto:\n");
+    printf("\n");
+    printf("\tSIG-Custumer: Um sistema de controle de cliente\n");
+    printf("As empresas da atualidade utilizam complexos sistemas gerenciais para,\n");
+    printf("entre varias outras coisas, controlar seus clientes\n");
+    printf("alem de ajudar a conseguir novos clientes.\n");
+    printf("O presente projeto tem como objetivo propor um software que implementa\n");
+    printf("funcionalidades basicas relacionadas ao controle de clientes de uma empresa,\n");
+    printf("facilitando a gestao\n");
+    printf("\n\n");
+    printf("\t\tDiscentes/Autores do Projeto:\n\n");
+    printf("\tCharles Eduardo Araujo De Faria. Matricula: 20220042687\n");
+    printf("\tVictor Ryan Galvao Silva. Matricula: 20220035163\n");
+    printf("\n");
 
-    printf("\n"
-
-    "\t\tSobre o projeto:\n"
-    "\tSIG-Custumer: Um sistema de controle de cliente\n"
-    "As empresas da atualidade utilizam complexos sistemas gerenciais para, "
-    "entre v�rias outras fun��es, gerenciar e manter seus clientes, "
-    "al�m de ajudar a conseguir novos clientes. "
-    "O presente projeto tem como objetivo propor um software que implementa "
-    "funcionalidades b�sicas relacionadas ao controle de clientes de uma empresa, "
-    "facilitando a gest�o\n"
-    "\n\n"
-
-    "\t\tDiscentes/Autores do Projeto:\n"
-    "\tCharles Eduardo Ara�jo De Faria, matr�cula: 20220042687\n"
-    "\tVictor Ryan Galv�o Silva, matr�cula:20220035163\n"
-    "\n"
-    );
     getch(); // Aperte enter para continuar
-    return 0;
 }
