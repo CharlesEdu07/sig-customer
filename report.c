772e4ae1e2af6cb8d37fa6703c28d2b1bf8dbefb
Charles Eduardo <charleseduardofaria@gmail.com>
2022-12-02 23:58:53 -0300

    listando clientes em ordem alfabetica

--- report.c
+++ report.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "report.h"
 #include "customer.h"
@@ -16,13 +17,103 @@ int menu_report(void) {
     printf("\t\t||        Modulo Relatorios        ||\n");
     printf("\t\t=====================================\n");
     printf("\t\t-------------------------------------\n");
-    printf("\t\t|        1 - Listar Clientes        |\n");
+    printf("\t\t|     1 - Relatorio de Clientes     |\n");
     printf("\t\t-------------------------------------\n");
     printf("\t\t-------------------------------------\n");
-    printf("\t\t|        2 - Listar Produtos        |\n");
+    printf("\t\t|     2 - Relatorio de Produtos     |\n");
     printf("\t\t-------------------------------------\n");
     printf("\t\t-------------------------------------\n");
-    printf("\t\t|         3 - Listar Pedido         |\n");
+    printf("\t\t|      3 - Relatorio de Pedido      |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|           0 - Regressar           |\n");
+    printf("\t\t-------------------------------------\n");
+
+    printf("\nDigite a opcao desejada: ");
+    int op = read_numeric_op();
+
+    return op;
+}
+
+int menu_customer_report(void) {
+    terminal_clear();
+
+    printf("\t\t=====================================\n");
+    printf("\t\t||      Relatorio de Clientes      ||\n");
+    printf("\t\t=====================================\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|     1 - Listar Todos Clientes     |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   2 - Listar em Ordem Alfabetica  |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|      3 - Pedidos Por Cliente      |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   4 - Listar Clientes Deletados   |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|           0 - Regressar           |\n");
+    printf("\t\t-------------------------------------\n");
+
+    printf("\nDigite a opcao desejada: ");
+    int op = read_numeric_op();
+
+    return op;
+}
+
+int menu_product_report(void) {
+    terminal_clear();
+
+    printf("\t\t=====================================\n");
+    printf("\t\t||      Relatorio de Produtos      ||\n");
+    printf("\t\t=====================================\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|     1 - Listar Todos Produtos     |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   2 - Listar em Ordem Alfabetica  |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   3 - Listar Produtos Por Tipos   |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   4 - Listar Produtos Por Preco   |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|   5 - Listar Produtos Deletados   |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|           0 - Regressar           |\n");
+    printf("\t\t-------------------------------------\n");
+
+    printf("\nDigite a opcao desejada: ");
+    int op = read_numeric_op();
+
+    return op;
+}
+
+int menu_request_report(void) {
+    terminal_clear();
+
+    printf("\t\t=====================================\n");
+    printf("\t\t||      Relatorio de Pedidos       ||\n");
+    printf("\t\t=====================================\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|     1 - Listar Todos Pedidos      |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|    2 - Listar Pedidos Por Data    |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|        3 - Maior Quantidade       |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|        4 - Menor Quantidade       |\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t-------------------------------------\n");
+    printf("\t\t|    5 - Listar Pedidos Deletados   |\n");
     printf("\t\t-------------------------------------\n");
     printf("\t\t-------------------------------------\n");
     printf("\t\t|           0 - Regressar           |\n");
@@ -94,6 +185,120 @@ void list_customer(void) {
     
         free(customer);
     }
+}
+
+void show_found_customer(Customer_List* customer_list) {
+    printf("\nNome do cliente: %s\n", customer_list->name);
+    printf("CPF do cliente: %s\n", customer_list->cpf);
+    printf("Celular do cliente: %s\n", customer_list->phone);
+    printf("Email do cliente: %s\n", customer_list->email);
+    printf("Endereco do cliente: %s\n", customer_list->address);
+}
+
+void list_customer_alpha_order(void) {
+    FILE* file;
+
+    int lenght;
+
+    Customer* customer;
+
+    Customer_List* new_customer;
+    Customer_List* list;
+
+    if (access("customer.dat", F_OK) != -1) {
+        file = fopen("customer.dat", "rb");
+
+        if (file == NULL) {
+            printf("Erro ao abrir o arquivo.\n");
+            exit(1);
+        }
+
+        else {
+            list = NULL;
+
+            customer = (Customer*) malloc(sizeof(Customer));
+
+            while (fread(customer, sizeof(Customer), 1, file)) {
+                if (customer->deleted == 0) {
+                    new_customer = (Customer_List*) malloc(sizeof(Customer_List));
+
+                    lenght = strlen(customer->name) + 1;
+                    new_customer->name = (char*) malloc(lenght * sizeof(char));
+                    strcpy(new_customer->name, customer->name);
+
+                    lenght = strlen(customer->cpf) + 1;
+                    new_customer->cpf = (char*) malloc(lenght * sizeof(char));
+                    strcpy(new_customer->cpf, customer->cpf);
+
+                    lenght = strlen(customer->phone) + 1;
+                    new_customer->phone = (char*) malloc(lenght * sizeof(char));
+                    strcpy(new_customer->phone, customer->phone);
+
+                    lenght = strlen(customer->email) + 1;
+                    new_customer->email = (char*) malloc(lenght * sizeof(char));
+                    strcpy(new_customer->email, customer->email);
+
+                    lenght = strlen(customer->address) + 1;
+                    new_customer->address = (char*) malloc(lenght * sizeof(char));
+                    strcpy(new_customer->address, customer->address);
+
+                    if (list == NULL) {
+                        list = new_customer;
+                        list->next = NULL;
+                    }
+
+                    else if (strcmp(list->name, new_customer->name) > 0) {
+                        new_customer->next = list;
+                        list = new_customer;
+                    }
+
+                    else {
+                        Customer_List* before = list;
+                        Customer_List* current = list->next;
+
+                        while (current != NULL && strcmp(current->name, new_customer->name) < 0) {
+                            before = current;
+                            current = current->next;
+                        }
+
+                        before->next = new_customer;
+                        new_customer->next = current;
+                    }
+                }
+            }
+
+            free(customer);
+
+            new_customer = list;
+
+            while (new_customer != NULL) {
+                show_found_customer(new_customer);
+                
+                new_customer = new_customer->next;
+            }
+
+            new_customer = list;
+
+            while (new_customer != NULL) {
+                list = new_customer->next;
+
+                free(new_customer->name);
+                free(new_customer->cpf);
+                free(new_customer->phone);
+                free(new_customer->email);
+                free(new_customer->address);
+                free(new_customer);
+
+                new_customer = list;
+            }
+        }
+
+        fclose(file);
+    }
+}
+
+void list_request_by_customer(void) {
+    ;
 }
 
 void list_product(void) {
@@ -229,17 +434,17 @@ void mod_report(void) {
     while (op != 0) {
         switch (op) {
             case 1:
-                list_customer();
+                mod_customer_report();
 
                 break;
 
             case 2:
-                list_product();
+                mod_product_report();
 
                 break;
 
             case 3:
-                list_request();
+                mod_request_report();
 
                 break;
 
@@ -252,5 +457,76 @@ void mod_report(void) {
         press_enter_to_continue();
 
         op = menu_report();
+    }
+}
+
+void mod_customer_report(void) {
+    int op = menu_customer_report();
+
+    while (op != 0) {
+        switch (op) {
+            case 1:
+                list_customer();
+
+                break;
+
+            case 2:
+                list_customer_alpha_order();
+
+                break;
+
+            default:
+                printf("\nOpcao invalida. Por favor, digite uma opcao valida\n");
+
+                break;
+        }
+
+        press_enter_to_continue();
+
+        op = menu_customer_report();
+    }
+}
+
+void mod_product_report(void) {
+    int op = menu_product_report();
+
+    while (op != 0) {
+        switch (op) {
+            case 1:
+                list_product();
+
+                break;
+
+            default:
+                printf("\nOpcao invalida. Por favor, digite uma opcao valida\n");
+
+                break;
+        }
+
+        press_enter_to_continue();
+
+        op = menu_product_report();
+    }
+}
+
+void mod_request_report(void) {
+    int op = menu_request_report();
+
+    while (op != 0) {
+        switch (op) {
+            case 1:
+                list_request();
+
+                break;
+
+            default:
+                printf("\nOpcao invalida. Por favor, digite uma opcao valida\n");
+
+                break;
+        }
+
+        press_enter_to_continue();
+
+        op = menu_request_report();
     }
 }
--- report.h
+++ report.h
@@ -1,6 +1,26 @@
+typedef struct customer_list Customer_List;
+
+struct customer_list {
+    char* name;
+    char* cpf;
+    char* phone;
+    char* email;
+    char* address;
+    Customer_List* next;
+};
+
 int menu_report(void);
+int menu_customer_report(void);
+int menu_product_report(void);
+int menu_request_report(void);
 
 void list_customer(void);
 void list_product(void);
 void list_request(void);
 void mod_report(void);
+
+void show_found_customer(Customer_List* customer);
+
+void mod_customer_report(void);
+void mod_product_report(void);
+void mod_request_report(void);
